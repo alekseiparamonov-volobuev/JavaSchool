218388abcd4ddafd148e40e9ec94e4bf661bb6b3
Alexey Paramonov <paramonov_tama@mail.ru>
2025-02-16 12:02:57 +0300

    KAFKA-4: доработка потребителя Kafka (отправка подтверждения об успешной обработке событий через топик обратного потока)

--- consumer/src/main/java/sbp/school/kafka/service/ConsumerService.java
+++ consumer/src/main/java/sbp/school/kafka/service/ConsumerService.java
@@ -5,12 +5,17 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.TopicPartition;
 import sbp.school.kafka.config.KafkaConsumerConfig;
+import sbp.school.kafka.config.KafkaProducerConfig;
 import sbp.school.kafka.config.KafkaProperties;
+import sbp.school.kafka.dto.AckDto;
 import sbp.school.kafka.dto.TransactionDto;
 
 import java.time.Duration;
+import java.time.ZoneOffset;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,25 +23,29 @@ import java.util.Map;
 @Slf4j
 public class ConsumerService extends Thread {
 
+    public static final String CHECKSUM_TRANSACTION_TOPIC = KafkaProperties.getReverseTransactionTopic();
+    private final KafkaConsumer<String, TransactionDto> consumer;
+    private final KafkaProducer<Long, AckDto> checksumProducer;
     private static final int RECORD_TO_COMMIT_COUNT = 150;
-    private final String groupId;
     private final Map<TopicPartition, OffsetAndMetadata> currentOffsets = new HashMap<>();
 
     public ConsumerService(String groupId) {
-        this.groupId = groupId;
-
+        this.consumer = KafkaConsumerConfig.getKafkaConsumer(groupId);
+        this.checksumProducer = KafkaProducerConfig.getChecksumProducer();
     }
 
     public void poll() {
         int counter = 0;
-        KafkaConsumer<String, TransactionDto> consumer = KafkaConsumerConfig.getKafkaConsumer(groupId);
         try {
             consumer.subscribe(List.of(KafkaProperties.getTransactionTopic()));
             while (true) {
                 ConsumerRecords<String, TransactionDto> records = consumer.poll(Duration.ofMillis(100));
                 for (ConsumerRecord<String, TransactionDto> record : records) {
+                    TransactionDto transactionDto = record.value();
                     log.info("topic = {}, partition = {}, offset = {}, groupId = {}, value = {}",
-                            record.topic(), record.partition(), record.offset(), consumer.groupMetadata().groupId(), record.value());
+                            record.topic(), record.partition(), record.offset(), consumer.groupMetadata().groupId(), transactionDto);
+                    sendChecksum(transactionDto.getOperationTime().toInstant(ZoneOffset.UTC).toEpochMilli(),
+                            new AckDto(transactionDto.getId(), transactionDto.hashCode()));
                     currentOffsets.put(new TopicPartition(record.topic(), record.partition()),
                             new OffsetAndMetadata(record.offset() + 1));
                     if (counter % RECORD_TO_COMMIT_COUNT == 0) {
@@ -57,5 +66,21 @@ public class ConsumerService extends Thread {
     @Override
     public void run() {
         poll();
+    }
+
+    private void sendChecksum(Long operationTime, AckDto ackDto) {
+        log.info("Отправляю контрольную сумму: key={}, value={}", operationTime, ackDto);
+        checksumProducer.send(new ProducerRecord<>(
+                CHECKSUM_TRANSACTION_TOPIC,
+                operationTime,
+                ackDto
+        ), (metadata, exception) -> {
+            if (exception != null) {
+                log.error("Ошибка при отправке контрольной суммы: {}", exception.getMessage());
+            } else {
+                log.debug("Контрольная сумма успешно отправлена: offset={}, partition={}",
+                        metadata.offset(), metadata.partition());
+            }
+        });
     }
 }
