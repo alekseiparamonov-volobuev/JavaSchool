a97d9e71b2755336f9db375c57ac935792674904
Alexey Paramonov <paramonov_tama@mail.ru>
2025-02-22 12:06:27 +0300

    KAFKA-4: правки по код-ревью (отправка консьюмером чексумм) - отправка пачками, колбэк в метод

--- consumer/src/main/java/sbp/school/kafka/service/ConsumerService.java
+++ consumer/src/main/java/sbp/school/kafka/service/ConsumerService.java
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.TopicPartition;
 import sbp.school.kafka.config.KafkaConsumerConfig;
 import sbp.school.kafka.config.KafkaProducerConfig;
@@ -19,19 +20,34 @@ import java.time.ZoneOffset;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class ConsumerService extends Thread {
 
-    public static final String CHECKSUM_TRANSACTION_TOPIC = KafkaProperties.getReverseTransactionTopic();
+    private static final String CHECKSUM_TRANSACTION_TOPIC = KafkaProperties.getReverseTransactionTopic();
+    private static final int INITIAL_DELAY = 5;
+    private static final int PERIOD = 5;
     private final KafkaConsumer<String, TransactionDto> consumer;
     private final KafkaProducer<Long, AckDto> checksumProducer;
     private static final int RECORD_TO_COMMIT_COUNT = 150;
+    private final ScheduledExecutorService executorService;
     private final Map<TopicPartition, OffsetAndMetadata> currentOffsets = new HashMap<>();
+    private final Map<Long, AckDto> transactionsToSendCheckSums;
+    private final Runnable task;
 
     public ConsumerService(String groupId) {
         this.consumer = KafkaConsumerConfig.getKafkaConsumer(groupId);
         this.checksumProducer = KafkaProducerConfig.getChecksumProducer();
+        this.transactionsToSendCheckSums = new ConcurrentHashMap<>();
+        this.executorService = Executors.newSingleThreadScheduledExecutor();
+        this.task = () -> {
+            transactionsToSendCheckSums.forEach(this::sendChecksum);
+            transactionsToSendCheckSums.clear();
+        };
     }
 
     public void poll() {
@@ -44,7 +60,7 @@ public class ConsumerService extends Thread {
                     TransactionDto transactionDto = record.value();
                     log.info("topic = {}, partition = {}, offset = {}, groupId = {}, value = {}",
                             record.topic(), record.partition(), record.offset(), consumer.groupMetadata().groupId(), transactionDto);
-                    sendChecksum(transactionDto.getOperationTime().toInstant(ZoneOffset.UTC).toEpochMilli(),
+                    transactionsToSendCheckSums.put(transactionDto.getOperationTime().toInstant(ZoneOffset.UTC).toEpochMilli(),
                             new AckDto(transactionDto.getId(), transactionDto.hashCode()));
                     currentOffsets.put(new TopicPartition(record.topic(), record.partition()),
                             new OffsetAndMetadata(record.offset() + 1));
@@ -65,6 +81,7 @@ public class ConsumerService extends Thread {
 
     @Override
     public void run() {
+        executorService.scheduleAtFixedRate(task, INITIAL_DELAY, PERIOD, TimeUnit.MINUTES);
         poll();
     }
 
@@ -74,13 +91,15 @@ public class ConsumerService extends Thread {
                 CHECKSUM_TRANSACTION_TOPIC,
                 operationTime,
                 ackDto
-        ), (metadata, exception) -> {
-            if (exception != null) {
-                log.error("Ошибка при отправке контрольной суммы: {}", exception.getMessage());
-            } else {
-                log.debug("Контрольная сумма успешно отправлена: offset={}, partition={}",
-                        metadata.offset(), metadata.partition());
-            }
-        });
+        ), this::callbackForChecksum);
+    }
+
+    private void callbackForChecksum(RecordMetadata metadata, Exception exception) {
+        if (exception != null) {
+            log.error("Ошибка при отправке контрольной суммы: {}", exception.getMessage());
+        } else {
+            log.debug("Контрольная сумма успешно отправлена: offset={}, partition={}",
+                    metadata.offset(), metadata.partition());
+        }
     }
 }
